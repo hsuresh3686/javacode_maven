3c7d3f2386d3bfabfd2923525c6fe46053696bbe
H SuresH K <hsuresh3686@gmail.com>
2020-02-22 13:44:34 +0000

    Maven Installation Instr. Commited

---
+++ maven_install.sh
@@ -0,0 +1,13 @@
+wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xf apache-maven-3.6.3-bin.tar.gz
+mv -f apache-maven-3.6.3 maven3
+
+echo "Maven Installed, open the sh file and set the properties"
+
+## Set below properties iin /etc/profile file under root
+##	vi /etc/profile
+##	export M2_HOME=/opt/maven3
+##	PATH=$PATH:$M2_HOME/bin
+##	export M2_HOME
+##	export PATH
+
